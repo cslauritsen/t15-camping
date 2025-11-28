85e764fc97d46a9ee975bafbbdfbf91b10bec9fb
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2025-11-28 17:15:45 -0500

    decruft

--- springboot-camping-server/src/main/java/com/shakertroop15/server/domain/qbo/address/EmailAddress.java
+++
@@ -1,5 +0,0 @@
-package com.shakertroop15.server.domain.qbo.address;
-
-public record EmailAddress(String address) {
-
-}
--- springboot-camping-server/src/main/java/com/shakertroop15/server/domain/qbo/address/QboAddress.java
+++
@@ -1,5 +0,0 @@
-package com.shakertroop15.server.domain.qbo.address;
-
-public record QboAddress(String id, String line1, String line2, String city, String countrySubDivisionCode, String country, String postalCode) {
-
-}
