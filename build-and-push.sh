cbc9a21a50a332c162acb389ea4e733a30342e13
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2025-11-28 21:02:29 -0500

    fix overwrite image with chart

--- build-and-push.sh
+++ build-and-push.sh
@@ -5,23 +5,42 @@ set -e
 
 tmp=$(mktemp -d)
 
+
+helmup() {
+
+  version=$1; shift
+
+  for chart in ./charts/*-backend-chart ./charts/*-ui-chart; do
+      pushd $chart
+      helm dep update --skip-refresh
+      helm package . --version=$version --app-version=$version -d $tmp
+      helm push $tmp/*.tgz oci://docker.io/cslauritsen
+      rm $tmp/*.tgz
+      popd
+  done
+
+  pushd charts/t15-camping
+  yq -i ".dependencies[] |= .version = \"$version\"" Chart.yaml
+  helm dep update --skip-refresh
+  helm package . --version=$version --app-version=$version -d $tmp
+  helm push $tmp/*.tgz oci://docker.io/cslauritsen
+  rm $tmp/*.tgz
+  popd
+
+}
+
 version=${VERSION?:Not set}
 git tag $version
 
 pushd springboot-camping-server
-helm package ./charts/t15-camping-backend --version=$version --app-version=$version -d $tmp
 ./gradlew clean bootBuildImage -Pversion=$version
 docker push cslauritsen/t15-camping-backend:$version
 popd
 
+
 pushd t15-camping-react-ui
-helm package ./charts/t15-camping-ui --version=$version --app-version=$version -d $tmp
 docker build --build-arg APP_VER=$version -t cslauritsen/t15-camping-frontend:$version .
 docker push cslauritsen/t15-camping-frontend:$version
 popd
 
-
-helm push $tmp/*.tgz oci://docker.io/cslauritsen
-rm $tmp/*.tgz
-helm package ./charts/t15-camping --version=$version --app-version=$version -d $tmp
-helm push $tmp/*.tgz oci://docker.io/cslauritsen
+helmup $version
