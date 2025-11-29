963419c2f8b192198d7330d7779689be004b428c
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2025-11-28 20:29:42 -0500

    --amend

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,21 +3,25 @@
 
 set -e
 
-$tmp=$(mktemp -d)
+tmp=$(mktemp -d)
 
 version=${VERSION?:Not set}
 git tag $version
 
 pushd springboot-camping-server
-helm package ./helm/t15-camping-backend --version=$version --app-version=$version -d $tmp
+helm package ./charts/t15-camping-backend --version=$version --app-version=$version -d $tmp
 ./gradlew clean bootBuildImage -Pversion=$version
 docker push cslauritsen/t15-camping-backend:$version
 popd
 
 pushd t15-camping-react-ui
-helm package ./helm/t15-camping-ui --version=$version --app-version=$version -d $tmp
+helm package ./charts/t15-camping-ui --version=$version --app-version=$version -d $tmp
 docker build --build-arg APP_VER=$version -t cslauritsen/t15-camping-frontend:$version .
 docker push cslauritsen/t15-camping-frontend:$version
 popd
 
+
+helm push $tmp/*.tgz oci://docker.io/cslauritsen
+rm $tmp/*.tgz
+helm package ./charts/t15-camping --version=$version --app-version=$version -d $tmp
 helm push $tmp/*.tgz oci://docker.io/cslauritsen
